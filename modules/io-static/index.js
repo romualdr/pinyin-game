e3998cb01de4a0502bdd51f6850fdaac46812313
Romuald R <romuald.ribas@epitech.eu>
2014-11-03 15:36:31 +0800

    Removed unused things

--- modules/io-static/index.js
+++
@@ -1,9 +0,0 @@
-var serveStatic = require('serve-static');
-
-module.exports = ['http', function (io, http) {
-	var _plugin = io.register('static');
-
-	_plugin('serve', function (folder) {
-		http._app.use(serveStatic(folder));
-	});
-}];
